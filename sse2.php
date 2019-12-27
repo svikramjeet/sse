eb6c5e46cbbcd81dc0a127505b7617a36feef672
svikramjeet <vikramjeet@ucreate.co.in>
2019-12-27 12:46:55 +0530

    added sse2

---
+++ sse2.php
@@ -0,0 +1,23 @@
+<?php
+	require_once('vendor/autoload.php'); //Load with ClassLoader
+	
+	use Sse\Event;
+	use Sse\SSE;
+	
+	//create the event handler
+	class YourEventHandler implements Event {
+		public function update(){
+			//Here's the place to send data
+			return 'Hello, world!';
+		}
+		
+		public function check(){
+			//Here's the place to check when the data needs update
+			return true;
+		}
+	}
+	
+	$sse = new SSE(); //create a libSSE instance
+	$sse->addEventListener('event_name', new YourEventHandler());//register your event handler
+	$sse->start();//start the event loop
+	
